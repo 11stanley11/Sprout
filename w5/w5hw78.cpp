8a4f0e14fbb8c425ab693d4e0f6ee3cf6238a552
11stanley11 <stanleyganger@gmail.com>
2024-04-13 15:08:44 +0800

    #78 done

--- w5/w5hw78.cpp
+++ w5/w5hw78.cpp
@@ -1,3 +1,4 @@
+// https://neoj.sprout.tw/problem/78/
 #pragma GCC optimize ("O2")
 #pragma GCC optimize ("O3")
 #pragma GCC optimize ("Ofast")
@@ -18,19 +19,41 @@ using namespace std;
 #define input freopen("input.in", "r", stdin)
 
 int main() {
-    input; // annotate before submitting
+    // input; // annotate before submitting
     boost;
     
-    int t;
-    int gift[7];
+    int t, ans;
+    int gift[6];
     cin >> t;
     while(t--) {
-        rep(i, 1, 7, 1) {
+        ans = 0;
+        rep(i, 0, 6, 1) {
             cin >> gift[i];
         }
-        rep(i, 6, 0, -1) {
-            
+        ans += gift[5];
+        ans += gift[4]; gift[0] -= gift[4] * 11;
+        ans += gift[3]; gift[1] -= gift[3] * 5; 
+        ans += gift[2] / 4; gift[2] %= 4; if(gift[2] > 0) ans++;
+        if(gift[2] == 1) {
+            gift[1] -= 5; gift[0] -= 7;
+        }else if(gift[2] == 2) {
+            gift[1] -= 3; gift[0] -= 6;
+        }else if(gift[2] == 3) {
+            gift[1] -= 1; gift[0] -= 5;
         }
+        if(gift[1] < 0) gift[0] += 4 * gift[1];
+        if(gift[1] > 0) {
+            ans += gift[1] / 9; gift[1] %= 9; 
+            if(gift[1] > 0) {
+                ans++; 
+                gift[0] -= 36 - 4 * gift[1];
+            }
+        }
+        if(gift[0] > 0) {
+            ans += gift[0] / 36; gift[0] %= 36; 
+            if(gift[0] > 0) ans++;
+        }
+        cout << ans << endl;
     }
     
     return 0;
