23ac4744e9a34b899a3793f84bb35934c098dec9
11stanley11 <stanleyganger@gmail.com>
2024-04-21 13:31:32 +0800

    #140 done

--- w7/w7hw140.cpp
+++ w7/w7hw140.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 #define rep(v, i, n, s) for(int v = i; s > 0 ? v < n : v > n; v += s)
 #define pb push_back
-#define ll long long
+#define int long long
 #define endl '\n'
 #define pii pair<int,int>
 #define mpii(a, b) make_pair(a, b)
@@ -18,11 +18,37 @@ using namespace std;
 #define debuq(x) cerr<<"debug:"<<#x<<endl;
 #define input freopen("../input.in", "r", stdin)
 
+const int MAXN = 1e5 + 1; 
+const int MOD = 1e6 + 7;
+int dp[MAXN][3][3]; //0 1 2 R G B
+int sum[MAXN];
+
 signed main() {
-    input; // annotate before submitting
+    // input; // annotate before submitting
     boost;
+
+    //bottom up
+    memset(sum, 0, sizeof(0));
+    rep(i, 0, 3, 1) rep(j, 0, 3 ,1) {
+        if(i == j) dp[1][i][j] = 1;
+        else dp[1][i][j] = 0;
+        sum[1] += dp[1][i][j]; 
+    }
+    rep(i, 2, MAXN, 1) rep(j, 0, 3, 1) {
+        dp[i][0][j] = (dp[i-1][0][j] + dp[i-1][1][j] + dp[i-1][2][j]) % MOD; // red
+        dp[i][1][j] = (dp[i-1][0][j] + dp[i-1][1][j]) % MOD; // blue
+        dp[i][2][j] = (dp[i-1][0][j] + dp[i-1][2][j]) % MOD; // green
+        sum[i] += (dp[i][0][j] + dp[i][1][j] + dp[i][2][j]) % MOD;
+    }
     
-    
+    int t, n, ans;
+    cin >> t;
+    while(t--) {
+        cin >> n;
+        ans = sum[n] - dp[n][1][2] - dp[n][2][1];
+        while(ans < 0) ans += MOD;
+        cout << ans % MOD << endl;
+    }
     
     return 0;
 }
